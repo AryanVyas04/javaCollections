bf072a4299402cb5a3c8173d2778b4eec78b25cf
AryaVyas04 <mukundjivyas@gmail.com>
2026-01-15 14:41:23 +0530

    Introduction addToList methood

--- src/main/java/com/DSA/Main.java
+++ src/main/java/com/DSA/Main.java
@@ -1,4 +1,8 @@
 package com.DSA;
+
+import java.util.ArrayList;
+import java.util.List;
+
 //My first commit
 //working on local branch
 public class Main {
@@ -7,5 +11,18 @@ public class Main {
         arrayList Arraylist = new arrayList();
         Arraylist.list1.add(1);
         System.out.println(Arraylist.list1.get(0));
+
+        //Adding elemenets in arraylist
+        List<Integer> addtoList = new ArrayList<>();
+        addtoList.add(1);  // add directly
+        addtoList.add(2);
+        addtoList.add(3);
+        addtoList.add(4);
+
+        //Accessing elements of arrayList
+        for(int i=0 ;i<addtoList.size();i++)
+        {
+            System.out.println(addtoList.get(i));
+        }
     }
 }
