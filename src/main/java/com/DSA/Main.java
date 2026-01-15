3b04a80abbea44f486b3711bcd35e7d1b391025e
AryaVyas04 <mukundjivyas@gmail.com>
2026-01-15 13:47:49 +0530

    initial commit

---
+++ src/main/java/com/DSA/Main.java
@@ -0,0 +1,11 @@
+package com.DSA;
+//My first commit
+//my sec
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        arrayList Arraylist = new arrayList();
+        Arraylist.list1.add(1);
+        System.out.println(Arraylist.list1.get(0));
+    }
+}
---
+++ src/main/java/com/DSA/arrayList.java
@@ -0,0 +1,17 @@
+package com.DSA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrayList {
+    //Create a arraylist
+    ArrayList list = new ArrayList(); // This will give warning as as ArrayList
+    // expects a genric data type
+
+    ArrayList<Integer> list1 = new ArrayList<>(12);
+    // we can change the initial capacity
+
+    List<Integer> list2 = new ArrayList<>();
+
+
+}
